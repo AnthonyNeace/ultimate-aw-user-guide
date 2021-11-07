1bcaa83d03c1a4f61909dd37f9a9a78ef8ccd534
Anthony Neace <hyperanthony@gmail.com>
2021-11-07 12:33:11 -0800

    Import AW 7.0 Help Topics (#5)
    * Set workspace settings
    
    * Update install script
    
    * Import 7.0 help topics
    
    * Update readme
    
    * Update for UTF16-LE diffs
    
    * Updated header
    
    * Convert tabs to spaces

--- install.ps1
+++ install.ps1
@@ -1,13 +1,37 @@
 # Download the Ultimate AW User Guide from GitHub and backup the current default.awh
+[CmdletBinding()]
 Param
 (
     [string] $AWHelpPath = "C:\Users\$env:username\AppData\Local\ActiveWorlds 3D\Help\",
-    [string] $Default = "default.awh"		
+    [string] $Default = "default.awh",
+    [Switch] $Remote = $false,
+    [Switch] $SkipBackup = $false
 )
 
-$timestamp = (Get-Date -f yyyyMMdd-HHmm)
+if($SkipBackup)
+{
+    Write-Debug "Skipping backup"
+}
+else 
+{
+    # Backup existing default.awh file in the same directory
+    $timestamp = (Get-Date -f yyyyMMdd-HHmm)
 
-# Create a backup of the existing help file.
-Move-Item "$AWHelpPath$Default" $AWHelpPath"backup-"$timestamp".awh" -Force
+    Write-Debug "Backing up $AWHelpPath$Default to $($AWHelpPath)backup-$($timestamp).awh"
 
-(Invoke-WebRequest -URI "https://raw.github.com/AnthonyNeace/AWUserGuideUtilities/master/UserGuides/ultimateawuserguide.awh").Content | Out-File "$AWHelpPath$Default" -Encoding "UTF8"
+    # Create a backup of the existing help file.
+    Move-Item "$AWHelpPath$Default" $AWHelpPath"backup-"$timestamp".awh" -Force
+}
+
+if($Remote)
+{
+    Write-Debug "Copying remote to $($AWHelpPath)default.awh"
+
+    (Invoke-WebRequest -URI "https://raw.github.com/AnthonyNeace/AWUserGuideUtilities/main/user-guides/ultimateawuserguide.awh").Content | Out-File "$AWHelpPath$Default" -Encoding "UTF8"
+}
+else 
+{
+    Write-Debug "Copying to $($AWHelpPath)default.awh"
+
+    Copy-Item ".\user-guides\ultimateawuserguide.awh" "$($AWHelpPath)default.awh" -Force
+}
